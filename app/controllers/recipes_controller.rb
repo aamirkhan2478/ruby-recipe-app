c90632e3b057491ad1fd5cfc8aa21cd1b9e957d0
aamir <aamir.khan2478@gmail.com>
2022-12-14 15:27:29 +0500

    Perform CRUD Opreation and also set public functionality.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,48 @@
 class RecipesController < ApplicationController
-  def create; end
+  def new
+    @recipe = Recipe.new
+  end
 
-  def index; end
+  def create
+    @user = current_user
+    @recipe = @user.recipes.new(recipes_params)
+    @recipe.public = false
+    if @recipe.save
+      redirect_to recipes_path, notice: 'recipe was successfully created'
+    else
+      render :new, alert: "Couldn't create recipe for user"
+    end
+  end
 
-  def show; end
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @foods = Food.all
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'Recipe was deleted successfully'
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.public == true
+      @recipe.update(public: false)
+    else
+      Recipe.find(@recipe.id).update(public: true)
+    end
+    redirect_to recipe_path(@recipe.id), notice: 'Public Updated'
+  end
+
+  private
+
+  def recipes_params
+    params.permit(:name, :description, :preparation_time, :cooking_time, current_user.id)
+  end
 end
--- db/migrate/20221212130312_create_recipes.rb
+++ db/migrate/20221212130312_create_recipes.rb
@@ -4,9 +4,9 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.datetime :preparation_time
-      t.datetime :cooking_time
-      t.text :decription
+      t.string :preparation_time
+      t.string :cooking_time
+      t.text :description
       t.boolean :public
       t.references :user, null: false, foreign_key: true
 

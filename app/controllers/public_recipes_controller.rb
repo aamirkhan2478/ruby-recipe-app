edcc83a949463714a5ff1635d78b4c021510560e
lomantech <87063237+lomantech@users.noreply.github.com>
2022-12-16 11:22:51 +0300

    update and fixed bug

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @user = current_user
-    @recipes = @user.recipes.where(public: true).order(created_at: :desc)
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 end
